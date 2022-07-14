538480a7bdec387affe34d995ed1dd8209718955
Tanya Goyal <55893042+tanyagoyal3110@users.noreply.github.com>
2022-07-14 12:12:22 +0530

    Update jscript.js

--- todo/jscript.js
+++ todo/jscript.js
@@ -1,55 +1,44 @@
-// Create a "close" button and append it to each list item
-var myNodelist = document.getElementsByTagName("LI");
-var i;
-for (i = 0; i < myNodelist.length; i++) {
-  var span = document.createElement("SPAN");
-  var txt = document.createTextNode("\u00D7");
-  span.className = "close";
-  span.appendChild(txt);
-  myNodelist[i].appendChild(span);
-}
+todo__list = document.querySelector('.todo__list')
+new__form = document.querySelector('.new__form')
+new__submit = document.querySelector('.new__submit')
 
-// Click on a close button to hide the current list item
-var close = document.getElementsByClassName("close");
-var i;
-for (i = 0; i < close.length; i++) {
-  close[i].onclick = function() {
-    var div = this.parentElement;
-    div.style.display = "none";
-  }
-}
-
-// Add a "checked" symbol when clicking on a list item
-var list = document.querySelector('ul');
-list.addEventListener('click', function(ev) {
-  if (ev.target.tagName === 'LI') {
-    ev.target.classList.toggle('checked');
-  }
-}, false);
-
-// Create a new list item when clicking on the "Add" button
-function newElement() {
-  var li = document.createElement("li");
-  var inputValue = document.getElementById("myInput").value;
-  var t = document.createTextNode(inputValue);
-  li.appendChild(t);
-  if (inputValue === '') {
-    alert("You must write something!");
-  } else {
-    document.getElementById("myUL").appendChild(li);
-  }
-  document.getElementById("myInput").value = "";
-
-  var span = document.createElement("SPAN");
-  var txt = document.createTextNode("\u00D7");
-  span.className = "close";
-  span.appendChild(txt);
-  li.appendChild(span);
-
-  for (i = 0; i < close.length; i++) {
-    close[i].onclick = function() {
-      var div = this.parentElement;
-      div.style.display = "none";
+function addTodos(e) {
+    e.preventDefault();
+    txt = new__form.new_text.value.trim()
+    html = `
+        <li class="todo__item">
+            <span class="todo__icon-tick material-icons">check_box_outline_blank</span>
+            <span class="todo__text">${txt}</span>
+            <span class="todo__icon-delete material-icons">delete_outline</span>
+        </li>
+    `
+    if (txt.length > 0) {
+        todo__list.innerHTML += html
+        new__form.reset()
     }
-  }
-} 
+}
+function deleteTodos(e) {
+    if (e.target.className.includes('todo__icon-delete')) {
+        e.target.parentElement.remove()
+    }
+}
+function markDone(e) {
+    if (e.target.classList.contains('todo__item')) {
+        todo__item = e.target
+    } else {
+        todo__item = e.target.parentElement
+    }
+    todo__item.classList.toggle('done')
+    if (todo__item.classList.contains('done')) {
+        todo__item.querySelector('.todo__icon-tick').innerHTML = 'check_box'
+        todo__item.querySelector('.todo__text').classList.add('cross-out')
+    } else {
+        todo__item.querySelector('.todo__icon-tick').innerHTML = 'check_box_outline_blank'
+        todo__item.querySelector('.todo__text').classList.remove('cross-out') 
+    }
+}
+
+todo__list.addEventListener('click', deleteTodos)
+todo__list.addEventListener('click', markDone)
+new__form.addEventListener('submit', addTodos)
+new__submit.addEventListener('click', addTodos)
